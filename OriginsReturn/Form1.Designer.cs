cac11f244579f0934654aa800ae2f4e97c3f18d9
Greflop <clement.poinson@epita.fr>
2015-04-25 22:56:24 +0200

    Nothing

--- OriginsReturn/Form1.Designer.cs
+++ OriginsReturn/Form1.Designer.cs
@@ -28,12 +28,227 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mine de Fer";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(188, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mine d\'Or";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(353, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mine de Cristal";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(517, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Extracteur d\'Hydrogène";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(675, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Centrale Thermique";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Niveau 0",
+            "Niveau 1",
+            "Niveau 2",
+            "Niveau 3",
+            "Niveau 4",
+            "Niveau 5",
+            "Niveau 6",
+            "Niveau 7",
+            "Niveau 8",
+            "Niveau 9",
+            "Niveau 10",
+            "Niveau 11",
+            "Niveau 12",
+            "Niveau 13",
+            "Niveau 14",
+            "Niveau 15"});
+            this.comboBox1.Location = new System.Drawing.Point(23, 92);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Niveau 0",
+            "Niveau 1",
+            "Niveau 2",
+            "Niveau 3",
+            "Niveau 4",
+            "Niveau 5",
+            "Niveau 6",
+            "Niveau 7",
+            "Niveau 8",
+            "Niveau 9",
+            "Niveau 10",
+            "Niveau 11",
+            "Niveau 12",
+            "Niveau 13",
+            "Niveau 14",
+            "Niveau 15"});
+            this.comboBox2.Location = new System.Drawing.Point(191, 92);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Niveau 0",
+            "Niveau 1",
+            "Niveau 2",
+            "Niveau 3",
+            "Niveau 4",
+            "Niveau 5",
+            "Niveau 6",
+            "Niveau 7",
+            "Niveau 8",
+            "Niveau 9",
+            "Niveau 10",
+            "Niveau 11",
+            "Niveau 12",
+            "Niveau 13",
+            "Niveau 14",
+            "Niveau 15"});
+            this.comboBox3.Location = new System.Drawing.Point(356, 92);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 7;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Niveau 0",
+            "Niveau 1",
+            "Niveau 2",
+            "Niveau 3",
+            "Niveau 4",
+            "Niveau 5",
+            "Niveau 6",
+            "Niveau 7",
+            "Niveau 8",
+            "Niveau 9",
+            "Niveau 10",
+            "Niveau 11",
+            "Niveau 12",
+            "Niveau 13",
+            "Niveau 14",
+            "Niveau 15"});
+            this.comboBox4.Location = new System.Drawing.Point(520, 92);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 8;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "Niveau 0",
+            "Niveau 1",
+            "Niveau 2",
+            "Niveau 3",
+            "Niveau 4",
+            "Niveau 5",
+            "Niveau 6",
+            "Niveau 7",
+            "Niveau 8",
+            "Niveau 9",
+            "Niveau 10",
+            "Niveau 11",
+            "Niveau 12",
+            "Niveau 13",
+            "Niveau 14",
+            "Niveau 15"});
+            this.comboBox5.Location = new System.Drawing.Point(678, 91);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 21);
+            this.comboBox5.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(829, 432);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "OriginsReturnTool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+
     }
 }
 
--- OriginsReturn/Form1.cs
+++ OriginsReturn/Form1.cs
@@ -15,5 +15,109 @@ namespace OriginsReturn
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string niveau = Convert.ToString(comboBox1);
+            switch (niveau)
+            {
+                case "Niveau 4":
+                    ;
+                    break;
+                case "Niveau 5":
+                    ;
+                    break;
+                case "Niveau 6":
+                    ;
+                    break;
+                case "Niveau 7":
+                    ;
+                    break;
+                case "Niveau 8":
+                    ;
+                    break;
+                case "Niveau 9":
+                    ;
+                    break;
+                case "Niveau 10":
+                    ;
+                    break;
+                case "Niveau 11":
+                    ;
+                    break;
+                case "Niveau 12":
+                    ;
+                    break;
+                case "Niveau 13":
+                    ;
+                    break;
+                case "Niveau 14":
+                    ;
+                    break;
+                case "Niveau 15":
+                    ;
+                    break;
+                default:
+                    ;
+                    break;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string niveau = Convert.ToString(comboBox1);
+            switch (niveau)
+            {
+                case "Niveau 4":
+                    ;
+                    break;
+                case "Niveau 5":
+                    ;
+                    break;
+                case "Niveau 6":
+                    ;
+                    break;
+                case "Niveau 7":
+                    ;
+                    break;
+                case "Niveau 8":
+                    ;
+                    break;
+                case "Niveau 9":
+                    ;
+                    break;
+                case "Niveau 10":
+                    ;
+                    break;
+                case "Niveau 11":
+                    ;
+                    break;
+                case "Niveau 12":
+                    ;
+                    break;
+                case "Niveau 13":
+                    ;
+                    break;
+                case "Niveau 14":
+                    ;
+                    break;
+                case "Niveau 15":
+                    ;
+                    break;
+                default:
+                    ;
+                    break;
+            }
+        }
     }
 }
